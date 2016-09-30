eab384dfce1f6ca13aface63655cb90ca1e2194d
fullmetalcloud <guevarae@seattleu.edu>
2016-09-29 18:49:26 -0700

    server_test_1_nodejs

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var app = express();
 var path = require('path');
 
 app.get('/', function(req, res) {
+	if()
+
 	res.sendFile(path.join(__dirname + '/index.html'))
 });
+app.use('/static', express.static(__dirname));
 app.listen(3000);
 
