3abf2ef500eb6f690c2a422c3139f1a5ebad85e3
fullmetalcloud <gueved90@gmail.com>
2016-09-29 19:59:09 -0700

    adjusting server.js

--- server.js
+++ server.js
@@ -2,18 +2,16 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-<<<<<<< HEAD
 app.set('port',3000);
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 var server = app.listen(app.get('port'),function() {
 	var port = server.address().port;
 	console.log('Stuff happening in '+ port);
-=======
+
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'))
->>>>>>> 781979605f41e56d34dcfb9cb902afa7ce6d3027
 });
 
 console.log('Running in testing server');
